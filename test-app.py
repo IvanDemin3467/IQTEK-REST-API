f13a7275806001ac4ad27c2ef29c54dbad4b6e5e
Ivan Demin <IvanDemin3467@gmail.com>
2021-09-09 13:08:24 +0600

    Lots of comments

--- test-app.py
+++ test-app.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
     print(delete('http://localhost:80/user/3'))  # delete existing
     print(get('http://localhost:80/users').json())  # show all
     # Rejected
-    print(get('http://localhost:80/user/3'))   # show one non existing
-    print(post('http://localhost:80/user/2?title=Mikhail%20Vasilevich%20Lomonosov'))   # add at the place of existing
-    print(patch('http://localhost:80/user/3?title=Just%20Lomonosov'))  # modify non existing
-    print(delete('http://localhost:80/user/3'))  # delete non existing
+    print(get('http://localhost:80/user/3').text)   # show one non existing
+    print(post('http://localhost:80/user/2?title=Mikhail%20Vasilevich%20Lomonosov').text)   # add at the place of existing
+    print(patch('http://localhost:80/user/3?title=Just%20Lomonosov').text)  # modify non existing
+    print(delete('http://localhost:80/user/3').text)  # delete non existing
     delete('http://localhost:80/user/2')  # delete all
     delete('http://localhost:80/user/1')
     print(get('http://localhost:80/users').text)  # show empty DB
