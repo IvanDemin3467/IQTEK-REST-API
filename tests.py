1c8632390de9a6483bf5e9b640c1cce7ecec75bb
IvanDemin3467 <IvanDemin3467@gmail.com>
2021-09-10 07:48:12 +0600

    Some minor changes in test.py

--- tests.py
+++ tests.py
@@ -1,12 +1,15 @@
 from requests import post, get, patch, delete
 
+def show_all():
+    print("\nПолучить все записи")
+    result = get('http://localhost:80/users')
+    print(result.json())
+
 if __name__ == '__main__':
 
     # Successfull
 
-    print("\nПолучить все записи")
-    result = get('http://localhost:80/users')
-    print(result.json())
+    show_all()
 
     print("\nПолучить одну запись с id=2")
     result = get('http://localhost:80/user/2')
@@ -16,25 +19,19 @@ if __name__ == '__main__':
     result = post('http://localhost:80/user/3?title=%27%3B+DROP+TABLE+IF+EXISTS+users%3B+--')  # SQL injection
     print(result.status_code)
 
-    print("\nПолучить все записи")
-    result = get('http://localhost:80/users')
-    print(result.json())
+    show_all()
 
     print("\nИзменить запись с id=3")
     result = patch('http://localhost:80/user/3?title=Mikhail%20Vasilevich%20Lomonosov')
     print(result.status_code)
 
-    print("\nПолучить все записи")
-    result = get('http://localhost:80/users')
-    print(result.json())
+    show_all()
 
     print("\nУдалить запись с id=3")
     result = delete('http://localhost:80/user/3')
     print(result.status_code)
 
-    print("\nПолучить все записи")
-    result = get('http://localhost:80/users')
-    print(result.json())
+    show_all()
 
     # Rejected
 
