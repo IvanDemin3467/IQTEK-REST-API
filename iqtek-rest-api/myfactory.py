ae8b2eaf560084c8716c5be45b0a9583e6842f8c
Ivan Demin <IvanDemin3467@gmail.com>
2021-09-14 11:33:41 +0600

    Factory improved. Private property with @property decorator implemented

--- iqtek-rest-api/myfactory.py
+++ iqtek-rest-api/myfactory.py
@@ -36,7 +36,7 @@ class DictChecker:
             else:
                 raise TypeError(f"User init error. Key '{self.key_name}' must be in {self.dict_name} dictionary")
         else:
-            raise TypeError(f"User init error. Given {self.dict_name} structure must be dictionary")
+            raise TypeError(f"User init error. Given {self.dict_name} structure must be {dict}")
 
     def __get__(self, instance, class_):
         return instance.__dict__[self.dict_name]
@@ -86,12 +86,13 @@ class AbstractFactory(ABC):
     Интерфейс Абстрактной фабрики для создания сущностей Entity.
     Определяет абстрактные методы:
         create() для создания сущности с переданными параметрами;
-        create_empty() для создания пустой сущности (для возврата из репозитория при ошибочных переданных значениях)
+        create_empty() классовый метод для создания пустой сущности
+            для возврата из репозитория при ошибочных переданных параметрах
         get_factory_name() для возвращения имени фабрики (может потребоваться, чтобы дать имя таблице в базе данных)
-    Определяет классовую переменную empty_entity, в которой хранится инстанс пустой сущности
-        (чтобы не создавать многократно)
+    Определяет классовую переменную
+        __empty_entity, в которой хранится инстанс пустой сущности (чтобы не создавать многократно)
     """
-    empty_entity: Entity
+    __empty_entity: Entity
 
     @abstractmethod
     def create(self, entity_id: int, properties: dict) -> Entity:
@@ -110,46 +111,60 @@ class AbstractFactory(ABC):
 class UserFactory(AbstractFactory):
     """
     Конкретная реализация абстрактный фабрики. Предназначена для работы с сущностями User
+        create() - создаёт пользователя по переданным параметрам. Проверяет валидность параметров
+        get_factory_name() - возвращает имя фабрики "users" (может быть использовано в базе данных)
+        empty_entity
     """
 
     def __init__(self):
         """
-        Инициализирует классовую переменную empty_entity.
+        Инициализирует классовую переменную __empty_entity.
         Она используется для возвращения несуществующих сущностей из фабрики или из репозитория
         """
         UserFactory.create_empty()
+
+    @property
+    def empty_entity(self) -> Entity:
+        """
+        Дескриптор, который делает классовое свойство empty_entity приватным (readonly)
+        :return: возвращает содержимое __empty_entity, то есть, пустую сущность User
+        """
+        return self.__empty_entity
 
     def create(self, user_id: int, properties: dict) -> Entity:
         """
         Создаёт сущность User в соответствии с переданными параметрами
         :param user_id: целочисленное значение id пользователя
         :param properties: строковое значение ФИО пользователя
-        :return: объект User с заполненными параметрами
+        :return: объект User с заполненными параметрами, либо пустой User, если переданы неверные параметры
         """
-        entity = UserFactory.empty_entity
+        entity = UserFactory.__empty_entity
         try:
             entity = User(user_id, properties)
-        except TypeError as e:
-            print(e)
+        except TypeError as err:
+            print(err)
         return entity
 
     @classmethod
     def create_empty(cls) -> Entity:
         """
-        Создаёт сущность User с пустым ФИО и id=-1 и сохраняет его в классовую переменную empty_entity.
-        Такой id служит признаком несуществующей сущности
+        Классовый метод, который запускается при инициализации инстанса фабрики
+        Создаёт сущность User с пустым ФИО и id=-1 и сохраняет его в классовую переменную __empty_entity.
+            Такой id служит признаком несуществующей сущности
+            Эта переменная позже делается неизменяемой
+            Переменная сделана классовой, чтобы не создавать многократно
         :return: объект User с пустым ФИО и id=-1
         """
         entity = User(-1, {"title": ""})
-        cls.empty_entity = entity
+        cls.__empty_entity = entity
         return entity
 
     def get_factory_name(self) -> str:
         """
-        Позволяет узнать имя фабрики. В данном случае это "user"
+        Позволяет узнать имя фабрики. В данном случае это "users"
         :return: строковое значение имени фабрики
         """
-        return "user"
+        return "users"
 
 
 if __name__ == "__main__":
@@ -157,11 +172,51 @@ if __name__ == "__main__":
     Небольшой тест дескрипторов TypeChecker и DictChecker
     """
     factory = UserFactory()
-    user = factory.create(1, {"tit": "des"})
-    print(user.id, user.properties)
-    user = factory.create(1, ["title", "des"])
-    print(user.id, user.properties)
+    print("ID строковый вместо целочисленного")
     user = factory.create("1", {"title": "des"})
-    print(user.id, user.properties)
+    print(user.get_dict())
+
+    print("\nПередан список свойств вместо словаря")
+    user = factory.create(1, ["title", "des"])
+    print(user.get_dict())
+
+    print("\nПередан словарь с отсутствующим ключом 'title'")
+    user = factory.create(1, {"tit": "des"})
+    print(user.get_dict())
+
+    print("\nВсе параметры переданы верно")
     user = factory.create(1, {"title": "des"})
-    print("\nOk", user.id, user.properties)
+    print(user.get_dict())
+
+    """
+    Тест приватности свойства empty_entity
+    """
+
+    print("\nПолучение empty_entity")
+    empty = factory.empty_entity
+    print(empty.get_dict())
+
+    print("\nУстановка непустого значения empty_entity и повторный запрос на получение")
+    try:
+        factory.empty_entity = user
+    except AttributeError as e:
+        print(e)
+    empty = factory.empty_entity
+    print(empty.get_dict())
+
+    print("\nПопытка непосредственного обращения к приватному свойству")
+    try:
+        empty = factory.__empty_entity
+        print(empty.get_dict())
+    except AttributeError as e:
+        print(e)
+
+    print("\nПопытка непосредственного изменения приватного свойства приводит к созданию runtime приватного свойства")
+    print("но не изменяет настоящее классовое свойство")
+    try:
+        factory.__empty_entity = user
+        empty = factory.__empty_entity
+        print(empty.get_dict())
+        print(factory.empty_entity.get_dict())
+    except AttributeError as e:
+        print(e)
